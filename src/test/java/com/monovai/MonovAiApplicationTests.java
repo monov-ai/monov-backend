fdb6ec0d571852e35adec207698c9ab85c686b09
woals2840 <woals2840@gmail.com>
2026-03-18 17:08:03 +0900

    [chore] 프로젝트 기초 세팅

---
+++ src/test/java/com/monovai/MonovAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monovai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonovAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
